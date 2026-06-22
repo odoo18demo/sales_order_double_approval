12f91d36ef3ff6406c071dcbaa9496aa4e5f0057
adil-skysecretary <adil@leadanalytical.com>
2026-06-22 12:18:36 +0530

    just update the phone -delivery

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -6,6 +6,6 @@ class StockPicking(models.Model):
     driver_name = fields.Many2one('fleet.driver',string="Driver")
     driver_mobile = fields.Char(related='driver_name.phone', store=True, string="Driver Mobile")
     truck_plate_no = fields.Char("Truck Plate No")
-    customer_phone = fields.Char(related='partner_id.phone', store=True, string="Customer Phone")
+    customer_phone = fields.Char(string="Customer Phone")
     city_code = fields.Char("City Code")
     leave_time = fields.Float(string="Leave Time")
