e0442a3232f6982d100171b6b7c7529a1591035d
adil-skysecretary <adil@leadanalytical.com>
2026-06-22 12:30:20 +0530

    ..

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -4,7 +4,7 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     driver_name = fields.Many2one('fleet.driver',string="Driver")
-    driver_mobile = fields.Char(related='driver_name.phone', store=True, string="Driver Mobile")
+    driver_mobile = fields.Char(string="Driver Mobile")
     truck_plate_no = fields.Char("Truck Plate No")
     customer_phone = fields.Char(string="Customer Phone")
     city_code = fields.Char("City Code")
